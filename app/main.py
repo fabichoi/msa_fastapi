bc6fc52373d252ee47cda3b60aeb557d8ae497ad
fabichoi <fabianus.c@gmail.com>
2023-02-08 21:57:35 +0900

    기본 API 작성 완료 및 DB 추가 및 파일 구조 정리

--- app/main.py
+++ app/main.py
@@ -1,45 +1,20 @@
-from typing import List
+from fastapi import FastAPI
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from app.api.db import metadata, engine, database
+from app.api.movies import movies
 
+metadata.create_all(engine)
 app = FastAPI()
 
-fake_movie_db = [
-    {
-        'name': 'my movie',
-        'plot': 'my plot',
-        'genres': ['Action', 'Adventure', 'Fantasy'],
-        'casts': ['Tim', 'Kim', 'Fabian']
-    }
-]
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-class Movie(BaseModel):
-    name: str
-    plot: str
-    genres: List[str]
-    casts: List[str]
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
-@app.get('/')
-async def index():
-    return {"Real": "Python"}
-
-
-@app.post('/', status_code=201)
-async def add_movie(payload: Movie):
-    movie = payload.dict()
-    fake_movie_db.append(movie)
-    return {'id': len(fake_movie_db) - 1}
-
-
-@app.put('/{id}')
-async def update_movie(id: int, payload: Movie):
-    movie = payload.dict()
-    movies_length = len(fake_movie_db)
-    if 0 <= id <= movies_length:
-        fake_movie_db[id] = movie
-        return None
-
-    raise HTTPException(status_code=404, detail='Movie is not exists')
+app.include_router(movies)
